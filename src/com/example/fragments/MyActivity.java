d90da8fac095cdc770685756b1ba11c5213627be
Alexey Pastukhov <pastuhov1992@gmail.com>
2015-07-10 10:17:53 +0300

    Test with 2 fragments. Problem with replace

--- src/com/example/fragments/MyActivity.java
+++ src/com/example/fragments/MyActivity.java
@@ -1,7 +1,9 @@
 package com.example.fragments;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class MyActivity extends Activity {
@@ -9,10 +11,22 @@ public class MyActivity extends Activity {
      * Called when the activity is first created.
      */
     FragmentManager fragmentManager;
+    Fragment fragment1;
+    Fragment fragment2;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        fragment2 = new FragmentTest2();
+
         fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+
+
+        fragmentTransaction.replace(R.id.test, fragment2);
+        fragmentTransaction.addToBackStack(null);
+
+        fragmentTransaction.commit();
     }
 }
